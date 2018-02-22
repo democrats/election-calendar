d56470ef27155402b44de6123a21b3295befe49d
Peter Seibel <seibelp@dnc.org>
2018-02-21 19:21:51 -0800

    Add elections and early vote elements within the state G

--- web/js/calendar.js
+++ web/js/calendar.js
@@ -16,26 +16,17 @@
 
     let dateParser = d3.timeParse('%Y-%m-%d');
 
-
     // Graphical dimensions
     let totalWidth  = document.body.clientWidth;
     let totalHeight = window.innerHeight || document.body.clientHeight;
     let margin      = { top: 20, right: 40, bottom: 20, left: 20 };
     let width       = totalWidth - (margin.right + margin.left);
     let height      = totalHeight - (margin.top + margin.bottom);
-
-    let axisMargin = 60;
+    let axisMargin  = 60;
     let axisPadding = 10;
     let columnWidth = (width - (axisMargin + axisPadding)) / states.length;
 
-    let yScale = d3.scaleTime()
-        .domain([start, end])
-        .range([0, height - (margin.top + margin.bottom)]);
-
-    let yAxis = d3.axisLeft(yScale)
-        .tickFormat(d3.timeFormat("%B"))
-        .tickSize(-1 * (axisPadding + (columnWidth * states.length)));
-
+    // Tooltip
     var tooltip = d3.select('body').append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
@@ -49,18 +40,22 @@
       tooltip.transition().duration(500).style("opacity", 0);
     }
 
+    let yScale = d3.scaleTime().domain([start, end]).range([0, height - (margin.top + margin.bottom)]);
+
+    let yAxis = d3.axisLeft(yScale)
+        .tickFormat(d3.timeFormat("%B"))
+        .tickSize(-1 * (axisPadding + (columnWidth * states.length)));
+
+
     let svg = d3.select('body').append('svg')
         .attr('id', 'calendar')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
         .attr('width', width)
         .attr('height', height);
 
-    svg
-      .append('g')
-      .attr('id', 'timeline')
+    svg.append('g')
       .attr('transform', 'translate(' + axisMargin + ',' + margin.top + ')')
       .call(yAxis);
-
 
     function stateColumn(state) {
       return states.indexOf(state) * columnWidth;
@@ -69,66 +64,68 @@
     function setupStates(states) {
       let s = d3.select('#calendar').selectAll('g.state').data(states);
 
-      function colX (d) { return axisMargin + axisPadding + stateColumn(d); }
+      function x (d) { return axisMargin + axisPadding + stateColumn(d); }
 
-      let gs = s.enter().append('g')
-          .attr('id', function (d) { return d; })
+      let g = s.enter().append('g')
           .attr('class', 'state')
-          .attr('transform', function (d) { return 'translate(' + colX(d) + ',' + margin.top + ')'; })
+          .attr('transform', function (d) { return 'translate(' + x(d) + ',' + margin.top + ')'; })
           .on("mouseover", showTooltip)
           .on("mouseout", hideTooltip);
 
-      gs.append('rect')
+      g.append('rect')
         .attr('x', 0)
         .attr('y', 0)
         .attr('width', columnWidth - 4)
         .attr('height', yScale(end))
         .attr('fill', function (d) { return states.indexOf(d) % 2 == 0 ? 'rgba(0,0,256,0.05)' : 'rgba(256,0,0,0.05)'; })
 
-      gs.append('image')
+      g.append('image')
         .attr('href', function (d) { return 'images/' + d.toLowerCase() + '.png'; })
         .attr('x', 0)
         .attr('y', - margin.top)
         .attr('width', columnWidth - 4);
     }
 
-    function elections(election_dates) {
+    function extraElections(election_dates) {
+      let extra = []
       for (let state of states) {
-        election_dates.push({ state: state, label: 'federal_general_election', date: '2018-11-06' })
+        extra.push({ state: state, label: 'federal_general_election', date: '2018-11-06' })
       }
 
       if (false) {
-        election_dates.push({"state": "AK", "label": "federal_general_election", "date": "2018-01-01"});
-        election_dates.push({"state": "WY", "label": "federal_general_election", "date": "2018-01-01"});
+        extra.push({"state": "AK", "label": "federal_general_election", "date": "2018-01-01"});
+        extra.push({"state": "WY", "label": "federal_general_election", "date": "2018-01-01"});
       }
+      return extra;
+    }
 
-      let s = d3.select('#timeline').selectAll('g.election').data(election_dates);
+    function elections(election_dates) {
+      election_dates = election_dates.concat(extraElections());
 
-      function cx (d) { return axisPadding + stateColumn(d.state) + ((columnWidth - 4) / 2); }
-
-      s.enter()
-        .filter(function (d) { return states.indexOf(d.state) !== -1; })
-        .append('circle')
-        .attr('class', function (d) { return d.label.split('_').join(' '); })
-        .attr('r', 2)
-        .attr('cx', cx)
-        .attr('cy',  function (d) { return yScale(dateParser(d.date)); });
-
+      d3.select('#calendar').selectAll('g.state').each(function (state) {
+        d3.select(this).selectAll('circle.election').data(election_dates)
+          .enter()
+          .filter(function (d) { return d.state == state; })
+          .append('circle')
+          .attr('class', function (d) { return d.label.split('_').join(' '); })
+          .attr('r', 2)
+          .attr('cx', (columnWidth - 4)/2)
+          .attr('cy',  function (d) { return yScale(dateParser(d.date)); });
+      });
     }
 
     function earlyVotes(early_vote) {
-      let s = d3.select('#timeline').selectAll('rect.early_vote').data(early_vote);
-
-      function cx (d) { return axisPadding + stateColumn(d.state) + ((columnWidth - 4) / 2); }
-
-      s.enter()
-        .filter(function (d) { return states.indexOf(d.state) !== -1; })
-        .append('rect')
-        .attr('class', function (d) { return 'early_vote ' + d.type; })
-        .attr('x', function (d) { return cx(d) - 2; })
-        .attr('y', function (d) { return yScale(dateParser(d.start)); })
-        .attr('width', 4)
-        .attr('height', function (d) { return yScale(dateParser(d.end)) - yScale(dateParser(d.start)); })
+      d3.select('#calendar').selectAll('g.state').each(function (state) {
+        d3.select(this).selectAll('rect.early_vote').data(early_vote)
+          .enter()
+          .filter(function (d) { return d.state == state; })
+          .append('rect')
+          .attr('class', function (d) { return 'early_vote ' + d.type; })
+          .attr('x', function (d) { return ((columnWidth - 4)/2) - 2; })
+          .attr('y', function (d) { return yScale(dateParser(d.start)); })
+          .attr('width', 4)
+          .attr('height', function (d) { return yScale(dateParser(d.end)) - yScale(dateParser(d.start)); })
+      });
 
     }
 
